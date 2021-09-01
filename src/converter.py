100dc11d132e155f71c7d23d89e588fdf89dcd83
maxwell-wayne-17 <maxwell.wayne.17@cnu.edu>
2021-09-01 19:46:20 -0400

    Fixed sentence logic, now need to fix line number logic

--- src/converter.py
+++ src/converter.py
@@ -4,25 +4,26 @@ def read_file(file_name):
 
     #list containing tab separated sentances with a number value at the end
     converted_lines = list()
+    #variable to track line number to merge relationship and line
+    line_num = 0
 
     #open the file parameter
     with open(file_name, 'r') as f:
 
         #variables to temporarily hold sentences
         sentence = ""
-        sub_sentence = ""
+        sub_sentence = "1\t"
         period_index = 0
 
         #iterate through lines
         for line in f:
 
+            line_num += 1
             sentence = line
-            #Need to add while period_index != -1, change logic here
+
             period_index = sentence.find(".")
             #if no period, go to next line and store current line in subsentence
             while period_index != -1:
-                # Cases where period may not be end of sentence
-                sentence_length = len(sentence)
 
                 # Case where period is end
                 if period_index >= len(sentence) - 1:
@@ -42,14 +43,14 @@ def read_file(file_name):
                 sub_sentence += "\t" + "-1"
                 # Add to list output
                 converted_lines.append(sub_sentence)
-                # Clear subsentence
-                sub_sentence = ""
+                # Clear subsentence ** NOT WORKING WITH SENTENCES THAT SPAN LINES **
+                sub_sentence = str(line_num) + "\t"
 
                 #Reset sentence and period index
-                sentence = sentence[period_index:len(sentence) - 1]
+                sentence = sentence[period_index:len(sentence)]
                 #Remove period if there
                 if (sentence[0] == "."):
-                    sentence = sentence[1:len(sentence) - 1]
+                    sentence = sentence[1:len(sentence)]
                 #Get next period index
                 period_index = sentence.find(".")
 
@@ -60,12 +61,12 @@ def read_file(file_name):
 
 test_list = read_file("../data/test.txt")
 correct_list = [
-    "Here\tis\tthe\tfirst\tsentence\t-1",
-    "Another\tsentence\tis\there\t-1",
-    "This\tsentence\tis\ton\ta\tnew\tline\t-1",
-    "Corner\tcase\tincludes\tthis,\tthat,\tother,\t(ect.)\t-1",
-    "Here\tis\tanother\tcorner\tcase\twith\ta\tquote\t\"test\tquote.\"\t-1",
-    "New\tsentence\tis\there\t-1"
+    "1\tHere\tis\tthe\tfirst\tsentence\t-1",
+    "1\tAnother\tsentence\tis\there\t-1",
+    "2\tThis\tsentence\tis\ton\ta\tnew\tline\t-1",
+    "2\tCorner\tcase\tincludes\tthis,\tthat,\tother,\t(ect.)\t-1",
+    "2\tHere\tis\tanother\tcorner\tcase\twith\ta\tquote\t\"test\tquote.\"\t-1",
+    "3\tNew\tsentence\tis\there\t-1"
 ]
 # ** test_list is omitting "is" from sentence that spans new line **
 print(test_list == correct_list)
